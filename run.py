4866b5d24e06af6a3a73405c4ae8a08a1b6955ea
aimanh04 h <aimanhawaemanoh04@gmail.com>
2024-09-14 16:23:43 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,13 +15,18 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user
+    Run a while loop to collect a valid string of data from the user
+
+    via the terminal, which must be a string of 6 numbers separated
+
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
     
         sales_data = data_str.split(",")
 
@@ -131,5 +135,3 @@ def main():
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
-
